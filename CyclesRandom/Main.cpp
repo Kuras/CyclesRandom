7844035d936c15d4acccd287c2a23eaeb6d2504a
Przemek Kura <kuraprzemek@gmail.com>
2013-12-16 21:39:24 +0100

    make new file with main function

---
+++ CyclesRandom/Main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 16 gru 2013
+ *      Author: Baraka
+ */
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main (void){
+
+	cout << "Generuj liczby losowe!\nPodaj liczbe\n";
+
+	return EXIT_SUCCESS;
+
+}
+
+
+
+
+
+
