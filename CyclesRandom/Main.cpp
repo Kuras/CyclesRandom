f6a3ea706c45f1f98c640dc324e8dc00bd287c9d
Przemek Kura <kuraprzemek@gmail.com>
2013-12-16 21:58:11 +0100

    create sequnace random numers but they have six lenght cycle! yah

--- CyclesRandom/Main.cpp
+++ CyclesRandom/Main.cpp
@@ -18,15 +18,22 @@ int main (void){
 	int number;
 	cout << "Generuj liczby losowe!\nPodaj liczbe\n";
 	cin >> number;
+	int i=0;
 	randomNumber = getRandomFromNoyman(number);
-	cout << randomNumber;
+	cout << randomNumber<< " ";
+	while (i<100) {
+		randomNumber = getRandomFromNoyman(randomNumber);
+		cout << randomNumber<< " ";
+		i++;
+	}
 	return EXIT_SUCCESS;
 
 }
 
 int getRandomFromNoyman(int number){
 	int randomNum;
-	randomNum = sqrt(number);
+	randomNum = (sqrt(number) * 1000);
+	randomNum = randomNum%1000;
 	return randomNum;
 }
 
