27238f73e3c3a1c79fb5a9b50c86acc6fb5c00f8
Przemek Kura <kuraprzemek@gmail.com>
2013-12-16 21:49:56 +0100

    create new function getRandomNumer

--- CyclesRandom/Main.cpp
+++ CyclesRandom/Main.cpp
@@ -7,17 +7,28 @@
 
 #include<iostream>
 #include<cstdlib>
+#include<cmath>
 using namespace std;
+
+int getRandomFromNoyman(int number);
 
 int main (void){
 
+	int randomNumber;
+	int number;
 	cout << "Generuj liczby losowe!\nPodaj liczbe\n";
-
+	cin >> number;
+	randomNumber = getRandomFromNoyman(number);
+	cout << randomNumber;
 	return EXIT_SUCCESS;
 
 }
 
-
+int getRandomFromNoyman(int number){
+	int randomNum;
+	randomNum = sqrt(number);
+	return randomNum;
+}
 
 
 
